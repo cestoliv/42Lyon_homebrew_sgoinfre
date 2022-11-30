cd0d13a0cb08a5e0a58e4d0a55fd2ce2c96527be
Olivier Cartier <me@cestoliv.com>
2022-11-30 10:03:41 +0100

    Update /sgoinfre path for 42Lyon

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@
 
 # Create symlink of .brew in personal sgoinfre
 rm -rf $HOME/.brew
-rm -rf /sgoinfre/goinfre/Perso/$USER/homebrew
-mkdir -p /sgoinfre/goinfre/Perso/$USER/homebrew
-chmod -R 700 /sgoinfre/goinfre/Perso/$USER
-ln -s /sgoinfre/goinfre/Perso/$USER/homebrew $HOME/.brew
+rm -rf /sgoinfre/Perso/$USER/homebrew
+mkdir -p /sgoinfre/Perso/$USER/homebrew
+chmod -R 700 /sgoinfre/Perso/$USER
+ln -s /sgoinfre/Perso/$USER/homebrew $HOME/.brew
 
 # Delete and reinstall Homebrew from Github repo on symlinked destination
 git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew
